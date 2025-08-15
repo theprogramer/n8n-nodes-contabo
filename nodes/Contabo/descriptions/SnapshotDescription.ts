5f0f2a215387cf6c05627283845b813883ab35fc
Thiago Miranda <theprogramer@theprogramer.com.br>
2025-08-15 15:42:03 -0300

    feat: order actions by name

--- nodes/Contabo/descriptions/SnapshotDescription.ts
+++ nodes/Contabo/descriptions/SnapshotDescription.ts
@@ -8,49 +8,48 @@ export const snapshotOperations: INodeProperties[] = [
 		noDataExpression: true,
 		default: 'getAll',
 		options: [
-			{
-				name: 'Get Many',
-				value: 'getAll',
-				description: 'Get Many Snapshots for an Instance',
-				action: 'Get many snapshot',
-			},
-			{
-				name: 'Get a Snapshot',
-				value: 'get',
-				description: 'Get an Snapshot',
-				action: 'Get an snapshot',
-			},
-			{
-				name: 'Create a Snapshot',
-				value: 'create',
-				description: 'Create a Snapshot',
-				action: 'Create a snapshot',
-			},
-			{
-				name: 'Update a Snapshot',
-				value: 'update',
-				description: 'Update a Snapshot',
-				action: 'Update a snapshot',
-			},
-			{
-				name: 'Delete a Snapshot',
-				value: 'delete',
-				description: 'Delete a Snapshot',
-				action: 'Delete a snapshot',
-			},
-			{
-				name: 'Revert a Snapshot',
-				value: 'revert',
-				description: 'Revert the Instance to a Snapshot',
-				action: 'Revert a snapshot',
-			},
-			{
-				name: 'Get Many Snapshot Audits',
-				value: 'snapshotAudits',
-				description: 'Get Many Snapshot Audits',
-				action: 'Get many snapshot audits',
-			},
-
+      {
+        name: 'Create a Snapshot',
+        value: 'create',
+        description: 'Create a Snapshot',
+        action: 'Create a snapshot',
+      },
+      {
+        name: 'Delete a Snapshot',
+        value: 'delete',
+        description: 'Delete a Snapshot',
+        action: 'Delete a snapshot',
+      },
+      {
+        name: 'Get a Snapshot',
+        value: 'get',
+        description: 'Get an Snapshot',
+        action: 'Get an snapshot',
+      },
+      {
+        name: 'Get Many',
+        value: 'getAll',
+        description: 'Get Many Snapshots for an Instance',
+        action: 'Get many snapshot',
+      },
+      {
+        name: 'Get Many Snapshot Audits',
+        value: 'snapshotAudits',
+        description: 'Get Many Snapshot Audits',
+        action: 'Get many snapshot audits',
+      },
+      {
+        name: 'Revert a Snapshot',
+        value: 'revert',
+        description: 'Revert the Instance to a Snapshot',
+        action: 'Revert a snapshot',
+      },
+      {
+        name: 'Update a Snapshot',
+        value: 'update',
+        description: 'Update a Snapshot',
+        action: 'Update a snapshot',
+      },
 		],
 		displayOptions: {
 			show: {
